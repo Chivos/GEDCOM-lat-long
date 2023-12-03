09bf659704839e5a6a92d34ad7377c1b9b56ea70
Chivos <sylvainblo@gmail.com>
2023-12-03 18:47:29 +0100

    Progress, first working issue

--- gedcom_mod.py
+++ gedcom_mod.py
@@ -1,46 +1,64 @@
 import os #pour lecture du dossier courant
 import re #pour regex
 
+def recherche_coord(coord_file_list, INSEE):
+    pattern_ville = r'\bPPLA.?\b.*\b' + re.escape(INSEE) + r'\b' #ligne qui contient PPLA / PPLA2 / ... (lieu habité de division administrative, pour ne pas s'accrocher aux hameaux, quartiers...) + le code INSEE
 
+    for line_number_coord, line_coord in enumerate(coord_file_list):
+        if re.search(pattern_ville, line_coord):
+            #line_number = index
+            #print("ligne trouvé FR.txt", line_number_coord)
+            break
+
+    ville = coord_file_list[line_number_coord]
+    pattern_lat_long = r'(-?\d{1,3}\.\d+)' #signe - optionnel, valeur de 1 à 3 chiffres, un point, et un ou plusieurs chiffres
+    lat_long = re.findall(pattern_lat_long, ville) #renvoi un liste avec lat puis long
+    return(lat_long)
+
+
+###################################################################################################
+###################  FIN FONCTIONS, DEBUT SCRIPT  ###################
+###################################################################################################
+
+##Adresse des fichiers cherchés dans dossier courant du script
 adresse_GED = os.path.dirname(os.path.realpath(__file__)) + "\\base.ged"
 adresse_coord = os.path.dirname(os.path.realpath(__file__)) + "\\FR.txt"
 adresse_GED_mod = os.path.dirname(os.path.realpath(__file__)) + "\\base_mod.ged"
 
+##Ouverture des fichiers et création d'une copie du GED  
 file_GED = open(adresse_GED, 'r')
 file_coord = open(adresse_coord, 'r')
 file_GED_mod = open(adresse_GED_mod, 'w')
 
-lines_GED = file_GED.readlines() #Fichier GED d'entrée sous forme de liste
-lines_coord = file_coord.readlines() #Fichier des coordonnées d'entrée sous forme de liste
+GED_file_list = file_GED.readlines() #Fichier GED d'entrée sous forme de liste
+coord_file_list = file_coord.readlines() #Fichier des coordonnées d'entrée sous forme de liste
+GED_mod_file_list = [] #Liste du GED complété
 
-for line_number_GED, line in enumerate(lines_GED):
-    INSEE = re.search(r"\D+(\d{5})\D+", line) #en série - caractères non digits, 1 ou plus ; 5 digits ; caractères non digits, 1 ou plus
-    if INSEE:
+nb_insertion = 0 #Variable pour compter le nombre d'insertion de coordonnées et corriger la dérive entre le fichier GED d'entrée et sa copie
+
+###################################################################################################
+###################  BOUCLE PRINCIPALE  ###################
+###################################################################################################
+
+for line_number_GED, line_GED in enumerate(GED_file_list):
+    GED_mod_file_list.append(line_GED) #copie de la ligne GED en cours
+
+    INSEE = re.search(r"\D+(\d{5})\D+", line_GED) #en série - caractères non digits, 1 ou plus ; 5 digits ; caractères non digits, 1 ou plus
+    
+    if INSEE: #Si un code INSEE est trouvé sur la ligne
         INSEE = INSEE.group(1)
         print("Code INSEE lu", INSEE)
-        print("Ligne associée", line_number_GED)
-        break
+        print("Ligne associée", line_number_GED+1)
         
-line_coord_number = None
-pattern_ville = r'\bPPLA.?\b.*\b' + re.escape(INSEE) + r'\b' #ligne qui contient PPLA / PPLA2 / ... (lieu habité de division administrative, pour ne pas s'accrocher aux hameaux, quartiers...) + le code INSEE
+        lat_long = recherche_coord(coord_file_list, INSEE)
+        
+        GED_mod_file_list.insert(line_number_GED+1 + nb_insertion*3, "3 MAP\n")
+        GED_mod_file_list.insert(line_number_GED+2 + nb_insertion*3, "4 LATI N" + lat_long[0] + "\n")
+        GED_mod_file_list.insert(line_number_GED+3 + nb_insertion*3, "4 LONG E" + lat_long[1] + "\n")
 
-for index, line_coord in enumerate(lines_coord, start=0):
-    if re.search(pattern_ville, line_coord):
-        line_number = index
-        print("ligne trouvé FR.txt", line_number)
-        break
+        nb_insertion += 1
 
 
-ville = lines_coord[line_number]
-pattern_lat_long = r'(-?\d{1,3}\.\d+)' #signe - optionnel, valeur de 1 à 3 chiffres, un point, et un ou plusieurs chiffres
-lat_long = re.findall(pattern_lat_long, ville) #renvoi un liste avec lat puis long
-print(lat_long)
-
-print("line_number", line_number)
-
-lines_GED.insert(line_number_GED+1, "3 MAP\n")
-lines_GED.insert(line_number_GED+2, "4 LATI N" + lat_long[0] + "\n")
-lines_GED.insert(line_number_GED+3, "4 LONG E" + lat_long[1] + "\n")
-
-for ligne_ecriture in lines_GED:
+##Ecriture fichier GED modifié, complété avec les coordonnées lat/long
+for ligne_ecriture in GED_mod_file_list:
     file_GED_mod.write(ligne_ecriture)
