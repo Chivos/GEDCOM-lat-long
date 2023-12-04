6cc374cf7402ef15dd8a3a1a260c9170694c3a6c
Chivos <sylvainblo@gmail.com>
2023-12-04 11:19:33 +0100

    update script traitement FR.txt

--- tools/reduc-FR.py
+++ tools/reduc-FR.py
@@ -22,10 +22,18 @@ file_coord_mod_list = [] #Liste du FR modifié
 
 for line_number, line in enumerate(coord_file_list):
 
-    lieu_administratif = re.search(r"PPL", line) 
+    lieu_administratif = re.search(r"PPL", line)
+
     
     if lieu_administratif: #Si un code PPLA ou PPLA2 ou... est trouvé sur la ligne
-        file_coord_mod_list.append(line) #copie de la ligne en cours
+        
+        ligne_ajoute = coord_file_list[line_number]
+        ligne_ajoute = re.sub(r"^\d*", "",ligne_ajoute)
+        ligne_ajoute = re.sub(r"^\W*", "",ligne_ajoute)
+        ligne_ajoute = re.sub(r"Europe.*", "" ,ligne_ajoute)
+        #pattern = r"[^.]\d{5}(.*)"
+        #ligne_ajoute = re.sub(pattern, r"\1" ,ligne_ajoute) #après code INSEE
+        file_coord_mod_list.append(ligne_ajoute) #copie de la ligne en cours
 
 
 ##Ecriture fichier GED modifié, complété avec les coordonnées lat/long
